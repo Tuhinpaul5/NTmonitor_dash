14727e045830f85061806b940eb29d40b70de864
Tuhinpaul5 <tuhin.paul.5tuhin@gmail.com>
2026-07-17 18:57:31 +0530

    fix(node): correct JSON tag for NodeName field in Node struct

--- models/node.go
+++ models/node.go
@@ -12,9 +12,10 @@ const (
 
 type Node struct {
 	ID        string    `gorm:"primaryKey;type:char(32);default:substr(gen_random_uuid()::text, 1, 32)" json:"id"`
-	NodeName   string   `json:"country"`
+	NodeName   string   `json:"node_name"`
 	GateWayId  string    `json:"gateway_id"`
 	Status    NodeStatus `gorm:"type:node_status_enum;not null;default:'active'" json:"status"`
+
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
